b80723daab1be35dd5d7361e0dd2fcbc92ee5cdb
dewimardanic <dewimardanic@gmail.com>
2021-12-29 16:32:52 +0800

    react basic done

---
+++ 11-React-Basic/soal-1/src/coba.js
@@ -0,0 +1,8 @@
+// let array = ["saya","akan","menajadi","seorang","developer"]
+
+// console.log(array.concat(""))
+
+var kataPertama = "ab"
+var kataKedua = "CD"
+
+console.log(kataPertama.concat(" "+kataKedua))
